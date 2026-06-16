0114c5a31f59827f93cb55e6e3749cb966e4b79a
AllanCFs <105823755+AllanCFs@users.noreply.github.com>
2026-06-16 20:12:38 -0300

    Create Repositorio class with animal and user lists

---
+++ Repositorio.java
@@ -0,0 +1,10 @@
+package com.adopethub.infra;
+import com.adopethub.domain.Animal;
+import com.adopethub.domain.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Repositorio {
+    public List<Animal> animais = new ArrayList<>();
+    public List<Usuario> usuarios = new ArrayList<>();
+}
